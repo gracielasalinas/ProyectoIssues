10bb63d7a3441a7b2b751e5a61015c258ddbb4c4
fpablocj <78938456+fpablocj@users.noreply.github.com>
2022-10-25 20:45:10 -0500

    Creación del módulo de predicción del heartrate
    Se creó el componente para detectar el ritmo cardiaco de una persona con redes neuronales

---
+++ detecciónritmo-cardíaco.py
@@ -0,0 +1,8 @@
+import numpy as np
+import opencv as cv
+
+#Este es el módulo de detección del ritmo cardíaco a traves de redes neuronales, tomando una porcion de la frente de la persona
+a=1
+b=1
+
+c=a+b
